4ca02eabd4258a3caf827605c28254ea14433c3f
wuwywu <676425940@qq.com>
2023-03-08 14:48:23 +0800

    调试GPU的使用

--- pytorch-study/torch基础代码学习/基础方法学习/PyTorch基础用法.py
+++ pytorch-study/torch基础代码学习/基础方法学习/PyTorch基础用法.py
@@ -17,24 +17,24 @@ dtype_cpu = torch.FloatTensor # uncomment if you are using CPU
 dtype_gpu = torch.cuda.FloatTensor # uncomment if you are using GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-a = torch.randn(2,3)
+# a = torch.randn(2,3)
 # print(a.type())     # torch.FloatTensor
 
 # 判断数据类型
 # print(isinstance(a, torch.FloatTensor))    # Trut
 
 # 默认是torch.cuda.FloatTensor类型32位浮点类型(gpu)
-a = a.cuda()
+# a = a.cuda()
 # print(a.type()) # torch.cuda.FloatTensor
 
-b = torch.randn(2,3).type(dtype_gpu)
+# b = torch.randn(2,3).type(dtype_gpu)
 # print(b.type()) # torch.cuda.FloatTensor
 
 # 标量
-b1 = torch.tensor(1.)
+# b1 = torch.tensor(1.)
 # print(b1.type())    # torch.FloatTensor
 
-b2 = torch.tensor(1)
+# b2 = torch.tensor(1)
 # print(b2.type())    # torch.LongTensor 64位整型
 
 # shape与size()用法相同
@@ -43,15 +43,19 @@ b2 = torch.tensor(1)
 # print(a.size())
 
 # 向量
-x1 = torch.tensor([1.2, 2.3])
+# x1 = torch.tensor([1.2, 2.3])
 # print(x1)    # torch.FloatTensor
-x2 = torch.FloatTensor(2)   # 随机初始化维度
+# x2 = torch.FloatTensor(2)   # 随机初始化维度
 # print(x2)    # torch.FloatTensor
-x3_np = np.ones(2)
+# x3_np = np.ones(2)
 # print(type(x3_np))  # numpy.ndarray
-x3 = torch.from_numpy(x3_np) # .type(dtype_gpu).to(device)
+# x3 = torch.from_numpy(x3_np) # .type(dtype_gpu).to(device)
 # print(x3.type())
-
+a = torch.zeros(100,100).type(dtype_cpu)
+# print(a)
+while True:
+    a += 1
+#     pass
 
 
 
