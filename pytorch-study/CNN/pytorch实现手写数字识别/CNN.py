aa267f48a6a3bd3347ea30577497e2f008685ecd
wuwywu <676425940@qq.com>
2023-05-02 22:19:15 +0800

    cnn

--- pytorch-study/CNN/pytorch实现手写数字识别/CNN.py
+++ pytorch-study/CNN/pytorch实现手写数字识别/CNN.py
@@ -1,5 +1,11 @@
 # wuyong@ccnu
 '''
+网络结构如下所示
+        conv - relu - conv- relu - pool -
+        conv - relu - conv- relu - pool -
+        conv - relu - conv- relu - pool -
+        affine - relu - dropout - affine - dropout - softmax
+
 1、数据批量化
 2、建立CNN网络
 '''
